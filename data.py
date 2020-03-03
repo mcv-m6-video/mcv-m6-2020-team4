Correct computation of mIoU

data.py
@@ -85,8 +85,10 @@ def generate_noisy_annotations(gt_bb):
     for i in args_to_keep:
         keep_bb.append(noisy_bb[i])
     '''
+    keep_bb = []
     keep_bb = copy.deepcopy(noisy_bb)
     # Change the 5% of the bounding boxes in the GT
+
     args_to_generate = int(len(gt_bb) * 0.2)
 
     lst_gt = [item[0] for item in gt_bb]
@@ -98,10 +100,10 @@ def generate_noisy_annotations(gt_bb):
 
         keep_bb.append([frame_to_insert, 'car', 0, new_bb[0], new_bb[1], new_bb[2], new_bb[3]])
 
-#    for i in range(0, len(keep_bb)):
-#        for j in range(0, 4):
-#            keep_bb[i][3 + j] = keep_bb[i][3 + j] + float(np.random.normal(0, 5, 1))
-#
+    for i in range(0, len(keep_bb)):
+        for j in range(0, 4):
+            keep_bb[i][3 + j] = keep_bb[i][3 + j] + float(np.random.normal(0, 30, 1))
+
     keep_bb = sorted(keep_bb, key=lambda x: x[0], reverse=False)
     '''
     return keep_bb

metrics/iou.py
@@ -29,7 +29,7 @@ def bbox_iou(bboxA, bboxB):
 
 def compute_miou_list_bb(frame_det_bb, frame_gt_bb):
     miou = 0
-    correct = 0
+    n_gt = len(frame_gt_bb)
     for f_det in frame_det_bb:
 
         ious = []
@@ -44,33 +44,31 @@ def compute_miou_list_bb(frame_det_bb, frame_gt_bb):
         arg_max = np.argmax(ious)
         if ious[arg_max] > 0.5:
             miou += np.max(ious)
-            correct += 1
             frame_gt_bb.pop(arg_max)
-    if correct == 0:
+    if n_gt == 0:
         return 0
     else:
-        return miou/correct
-    
-    
-    
-    
+        return miou/n_gt
+
+
+
+
 def frame_miou(frame_det_bb, frame_gt_bb, confidence):
-    
+
     if confidence:
         frame_det_bb = sorted(frame_det_bb, key=lambda x: x[-1], reverse=True)
         frame_det_bb = [item[:-1] for item in frame_det_bb]
         miou = compute_miou_list_bb(frame_det_bb, frame_gt_bb)
-        
+
     else:
         miou = 0
         #Random shuffle
         for i in range(0, 10):
-            shuffle(frame_det_bb)      
+            shuffle(frame_det_bb)
             miou += compute_miou_list_bb(copy.deepcopy(frame_det_bb), copy.deepcopy(frame_gt_bb))
-        miou = miou/10    
+        miou = miou/10
         #Sorted by area
 #       frame_det_bb = sorted(frame_det_bb, key=lambda x: (x[5]-x[3])*(x[5]-x[3]), reverse=True)
-#       compute_miou_list_bb(frame_det_bb, frame_gt_bb)    
-        
+#       compute_miou_list_bb(frame_det_bb, frame_gt_bb)
+
     return miou
-    