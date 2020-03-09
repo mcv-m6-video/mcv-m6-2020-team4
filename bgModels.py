d3a12a950aeea9085d474d98df5e8536d9fb4da9
Yael Tudela <yaeltudelabarroso@gmail.com>
2020-03-09 11:08:55 +0100

    task3 with 3 modes

--- bgModels.py
+++ bgModels.py
@@ -1,8 +1,10 @@
-import numpy as np
-import cv2
-from data import number_of_images_jpg
-import imageio
 import copy
+
+import cv2
+import imageio
+import numpy as np
+
+from data import number_of_images_jpg
 
 
 def bg_model_grayscale(frames_path):
@@ -13,27 +15,27 @@ def bg_model_grayscale(frames_path):
     It takes quite a long time...
     """
     video_n_frames = number_of_images_jpg(frames_path)
-    p25_frames = int(video_n_frames*0.25)
-    img = cv2.imread(frames_path+'/frame_0001.jpg',0)
+    p25_frames = int(video_n_frames * 0.25)
+    img = cv2.imread(frames_path + '/frame_0001.jpeg', 0)
     imga = np.zeros((p25_frames, np.shape(img)[0], np.shape(img)[1])).astype(np.float16())
     print('Reading frames ')
     for i in range(0, p25_frames):
-        #in openCV imread(path,0) the 0 is already grayscale!
-        imga[i,...] =  cv2.imread(frames_path+('/frame_{:04d}.jpg'.format(i+1)),0).astype(np.float16())
+        # in openCV imread(path,0) the 0 is already grayscale!
+        imga[i, ...] = cv2.imread(frames_path + ('/frame_{:04d}.jpeg'.format(i + 1)), 0).astype(np.float16())
 
     # mean
     print('Calculating mean .... (takes a while)')
-    mu = np.mean(imga, axis = 0, dtype = np.float64)
+    mu = np.mean(imga, axis=0, dtype=np.float64)
     # variance
     print('Calculating variance .... (takes a while)')
-    sigma = np.std(imga, axis = 0, dtype = np.float64)
+    sigma = np.std(imga, axis=0, dtype=np.float64)
 
     print('End')
     return mu, sigma
 
 
 def remove_bg(mu, sigma, alpha, frames_path, initial_frame, final_frame,
-              animation = False, denoise = False):
+              animation=False, denoise=False):
     """
     Save detected bb in the same format as GT which is:
         'frame', 'label', 'id', 'xtl','ytl','xbr','ybr'
@@ -41,38 +43,39 @@ def remove_bg(mu, sigma, alpha, frames_path, initial_frame, final_frame,
     c = 0
     detected_bb = []
     for i in range(initial_frame, final_frame):
-        #read image
-        img = cv2.imread(frames_path+('/frame_{:04d}.jpg'.format(i+1)),0).astype(np.float64())
+        # read image
+        img = cv2.imread(frames_path + ('/frame_{:04d}.jpeg'.format(i + 1)), 0).astype(np.float64())
 
         if i == initial_frame and animation:
-            sx, sy = np.int(np.shape(img)[0]/4), np.int(np.shape(img)[1]/4)
-            frames = np.zeros((final_frame-initial_frame, sx, sy)).astype(np.uint8())
+            sx, sy = np.int(np.shape(img)[0] / 4), np.int(np.shape(img)[1] / 4)
+            frames = np.zeros((final_frame - initial_frame, sx, sy)).astype(np.uint8())
 
         frame = np.zeros(np.shape(img)).astype(np.uint8())
-        frame[np.abs(img-mu)>= alpha*(sigma+2)] = 255
-        frame[np.abs(img-mu)< alpha*(sigma+2)] = 0
+        frame[np.abs(img - mu) >= alpha * (sigma + 2)] = 255
+        frame[np.abs(img - mu) < alpha * (sigma + 2)] = 0
         if animation:
-            frames[c,...] = cv2.resize(frame, (sy,sx))
+            frames[c, ...] = cv2.resize(frame, (sy, sx))
 
         if denoise:
-            frame = cv2.medianBlur(frame,7)
+            frame = cv2.medianBlur(frame, 7)
 
-        (_,contours,_) = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        (_, contours, _) = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         for contour in contours:
             # Open cv documentation:  (x,y) be the top-left coordinate
-            #of the rectangle and (w,h) be its width and height.
-            (x,y,w,h) = cv2.boundingRect(contour)
-            if w>10 and h>10:
-                detected_bb.append([i, 'car', 0, x, y, x+w, y+h])
-        c = c+1
+            # of the rectangle and (w,h) be its width and height.
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if w > 10 and h > 10:
+                detected_bb.append([i, 'car', 0, x, y, x + w, y + h])
+        c = c + 1
     if animation:
         imageio.mimsave('bg_removal_a{}.gif'.format(alpha), frames)
 
     return detected_bb
 
+
 def remove_adaptive_bg(mu_original, sigma_original, alpha, rho, frames_path,
-            initial_frame, final_frame, animation = False, denoise = False):
+                       initial_frame, final_frame, animation=False, denoise=False):
     """
     Save detected bb in the same format as GT which is:
         'frame', 'label', 'id', 'xtl','ytl','xbr','ybr'
@@ -82,37 +85,47 @@ def remove_adaptive_bg(mu_original, sigma_original, alpha, rho, frames_path,
     c = 0
     detected_bb = []
     for i in range(initial_frame, final_frame):
-        #read image
-        img = cv2.imread(frames_path+('/frame_{:04d}.jpg'.format(i+1)),0).astype(np.float64())
+        # read image
+        img = cv2.imread(frames_path + ('/frame_{:04d}.jpg'.format(i + 1)), 0).astype(np.float64())
 
         if i == initial_frame and animation:
-            sx, sy = np.int(np.shape(img)[0]/4), np.int(np.shape(img)[1]/4)
-            frames = np.zeros((final_frame-initial_frame, sx, sy)).astype(np.uint8())
+            sx, sy = np.int(np.shape(img)[0] / 4), np.int(np.shape(img)[1] / 4)
+            frames = np.zeros((final_frame - initial_frame, sx, sy)).astype(np.uint8())
 
         frame = np.zeros(np.shape(img)).astype(np.uint8())
-        frame[np.abs(img-mu)>= alpha*(sigma+2)] = 255
-        frame[np.abs(img-mu)< alpha*(sigma+2)] = 0
+        frame[np.abs(img - mu) >= alpha * (sigma + 2)] = 255
+        frame[np.abs(img - mu) < alpha * (sigma + 2)] = 0
 
-        #Update mu and sigma if needed
+        # Update mu and sigma if needed
         mu[frame == 0] = rho * img[frame == 0] + (1 - rho) * mu[frame == 0]
-        sigma[frame == 0] = np.sqrt(rho * np.power((img[frame == 0] - mu[frame == 0]), 2) + (1 - rho) * np.power(sigma[frame == 0], 2))
+        sigma[frame == 0] = np.sqrt(
+            rho * np.power((img[frame == 0] - mu[frame == 0]), 2) + (1 - rho) * np.power(sigma[frame == 0], 2))
 
         if animation:
-            frames[c,...] = cv2.resize(frame, (sy,sx))
+            frames[c, ...] = cv2.resize(frame, (sy, sx))
 
         if denoise:
-            frame = cv2.medianBlur(frame,7)
+            frame = cv2.medianBlur(frame, 7)
 
-        (_,contours,_) = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        (_, contours, _) = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         for contour in contours:
             # Open cv documentation:  (x,y) be the top-left coordinate
-            #of the rectangle and (w,h) be its width and height.
-            (x,y,w,h) = cv2.boundingRect(contour)
-            if w>10 and h>10:
-                detected_bb.append([i, 'car', 0, x, y, x+w, y+h])
-        c = c+1
+            # of the rectangle and (w,h) be its width and height.
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if w > 10 and h > 10:
+                detected_bb.append([i, 'car', 0, x, y, x + w, y + h])
+        c = c + 1
     if animation:
         imageio.mimsave('bg_removal_a{}_p{}.gif'.format(alpha, rho), frames)
 
     return detected_bb
+
+
+def bg_estimation(mode, **kwargs):
+    if mode == 'mog':
+        return cv2.createBackgroundSubtractorMOG2(varThreshold=kwargs['varThreshold'], detectShadows=True)
+    elif mode == 'knn':
+        return cv2.createBackgroundSubtractorKNN(dist2Threshold=kwargs['dist2Threshold'], detectShadows=True)
+    elif mode == 'gmg':
+        return cv2.bgsegm.createBackgroundSubtractorGMG(initializationFrames=kwargs['initializationFrames'])
--- week2.py
+++ week2.py
@@ -1,21 +1,23 @@
-import numpy as np
 import matplotlib.pylab as plt
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
-from data import read_xml_gt, FrameCapture, read_xml_gt_options
+from bgModels import bg_model_grayscale, remove_bg, remove_adaptive_bg, bg_estimation
+from data import read_xml_gt_options
 from data import save_frames, number_of_images_jpg, filter_gt
-from utils.visualization import animate_iou, animation_2bb, plot_animation
-from bgModels import bg_model_grayscale, remove_bg, remove_adaptive_bg
 from metrics.mAP import calculate_ap
+from utils.visualization import animation_2bb
+import cv2
+from glob import glob
 
 def main():
     print("Task 1")
-    #task1("datasets/ai_challenge_s03_c010-full_annotation.xml")
+    # task1("datasets/ai_challenge_s03_c010-full_annotation.xml")
     print("Task 2")
-    task2('datasets/AICity_data/train/S03/c010/data', "datasets/ai_challenge_s03_c010-full_annotation.xml")
+    task2('datasets/AICity_data/train/S03/c010/frames/', "datasets/ai_challenge_s03_c010-full_annotation.xml")
     print("Task 3")
 
     print("Task 4")
+
 
 def task1(gt_path):
     path = 'datasets/AICity_data/train/S03/c010'
@@ -25,55 +27,55 @@ def task1(gt_path):
     frames_path = 'datasets/AICity_data/train/S03/c010/data'
     video_n_frames = number_of_images_jpg(frames_path)
 
-    #this is very time consuming, we should avoid comuting it more than once.
+    # this is very time consuming, we should avoid comuting it more than once.
     mu, sigma = bg_model_grayscale(frames_path)
 
     # Gaussian plot in the slides
-    xx = np.linspace(0, 255,1000)
-    exp = 1/(sigma[561,712]*np.sqrt(2*np.pi))*np.exp(-(xx-mu[561,712])**2/(2*sigma[561,712]**2))
+    xx = np.linspace(0, 255, 1000)
+    exp = 1 / (sigma[561, 712] * np.sqrt(2 * np.pi)) * np.exp(-(xx - mu[561, 712]) ** 2 / (2 * sigma[561, 712] ** 2))
     plt.figure()
-    plt.plot(xx, exp, color = 'blue', label = 'Distribution')
-    plt.axvline(x = mu[561,712], color = 'red', label = 'Mean')
-    plt.axvline(x = mu[561,712]-(sigma[561,712]+2), linestyle = '--', color = 'green', label = 'Tolerance ' + r'$\alpha = 1$')
-    plt.axvline(x = mu[561,712]+(sigma[561,712]+2), linestyle = '--',color = 'green')
-    plt.axvline(x = mu[561,712]-3*(sigma[561,712]+2), linestyle = '--', color = 'purple', label = 'Tolerance ' + r'$\alpha = 5$')
-    plt.axvline(x = mu[561,712]+3*(sigma[561,712]+2), linestyle = '--',color = 'purple')
+    plt.plot(xx, exp, color='blue', label='Distribution')
+    plt.axvline(x=mu[561, 712], color='red', label='Mean')
+    plt.axvline(x=mu[561, 712] - (sigma[561, 712] + 2), linestyle='--', color='green',
+                label='Tolerance ' + r'$\alpha = 1$')
+    plt.axvline(x=mu[561, 712] + (sigma[561, 712] + 2), linestyle='--', color='green')
+    plt.axvline(x=mu[561, 712] - 3 * (sigma[561, 712] + 2), linestyle='--', color='purple',
+                label='Tolerance ' + r'$\alpha = 5$')
+    plt.axvline(x=mu[561, 712] + 3 * (sigma[561, 712] + 2), linestyle='--', color='purple')
     plt.legend()
 
-
-    alphas = [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7]
+    alphas = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7]
     aps7 = []
 
-    #Test for different alpha values
+    # Test for different alpha values
     for a in alphas:
-        det_bb = remove_bg(mu, sigma, a, frames_path, int(video_n_frames*0.25), video_n_frames,
-                               animation = False, denoise = True)
+        det_bb = remove_bg(mu, sigma, a, frames_path, int(video_n_frames * 0.25), video_n_frames,
+                           animation=False, denoise=True)
 
         gt_bb = read_xml_gt_options(gt_path, True, True)
 
-
-        ap = calculate_ap(det_bb, gt_bb, int(video_n_frames*0.25), video_n_frames, mode = 'area')
-        print(a,ap)
+        ap = calculate_ap(det_bb, gt_bb, int(video_n_frames * 0.25), video_n_frames, mode='area')
+        print(a, ap)
         aps7.append(ap)
 
     #
     #
-    animation_2bb('try', '.gif', gt_bb, det_bb, frames_path, 10, 100, int(video_n_frames*0.25) + 1,
+    animation_2bb('try', '.gif', gt_bb, det_bb, frames_path, 10, 100, int(video_n_frames * 0.25) + 1,
                   int(1920 / 4), int(1080 / 4))
 
-#
-#
-#animation_2bb('try', '.gif', gt_bb, det_bb, frames_path, 10, 100, int(video_n_frames*0.25) + 1,
-#              int(1920 / 4), int(1080 / 4))
 
+#
+#
+# animation_2bb('try', '.gif', gt_bb, det_bb, frames_path, 10, 100, int(video_n_frames*0.25) + 1,
+#              int(1920 / 4), int(1080 / 4))
 
 
 def task2(frames_path, gt_path):
     grid_search = False
     save_videos = True
-    #mu, sigma = bg_model_grayscale(frames_path)
-    #np.savetxt("mu.csv", mu, delimiter=",")
-    #np.savetxt("sigma.csv", sigma, delimiter=",")
+    mu, sigma = bg_model_grayscale(frames_path)
+    np.savetxt("mu.csv", mu, delimiter=",")
+    np.savetxt("sigma.csv", sigma, delimiter=",")
     gt_bb = read_xml_gt_options(gt_path, True, True)
 
     classes_to_keep = ['car', 'bike']
@@ -90,18 +92,19 @@ def task2(frames_path, gt_path):
         for alpha in alphas:
             mAP_same_alfa = []
             for rho in rhos:
-                det_bb = remove_adaptive_bg(mu, sigma, alpha, rho, frames_path, int(video_n_frames*0.25), video_n_frames)
-                mAP = calculate_ap(det_bb, gt_bb, int(video_n_frames*0.25), video_n_frames, mode = 'area')
+                det_bb = remove_adaptive_bg(mu, sigma, alpha, rho, frames_path, int(video_n_frames * 0.25),
+                                            video_n_frames)
+                mAP = calculate_ap(det_bb, gt_bb, int(video_n_frames * 0.25), video_n_frames, mode='area')
                 mAP_same_alfa.append(mAP)
                 print("Alpha: {:2f} | Rho: {:2f} | mAP: {:2f} |".format(alpha, rho, mAP))
             mAPs.append(mAP_same_alfa)
 
         # Plot the surface
-        X,Y = np.meshgrid(alphas, rhos)
+        X, Y = np.meshgrid(alphas, rhos)
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         surf = ax.plot_surface(X, Y, mAPs, rstride=0.1, cstride=0.1,
-                    cmap='viridis', edgecolor='none')
+                               cmap='viridis', edgecolor='none')
         ax.set_xlabel('Alpha')
         ax.set_ylabel('Rho')
         ax.set_zlabel('mAP')
@@ -113,6 +116,53 @@ def task2(frames_path, gt_path):
         alpha = 3.5
         rho = 0.2
         det_bb = remove_adaptive_bg(mu, sigma, alpha, rho, frames_path, 700, 800, animation=True)
+
+
+def task3():
+    ims = sorted(glob("datasets/AICity_data/train/S03/c010/frames/*.jpeg"))
+
+    gt_bb = read_xml_gt_options('datasets/ai_challenge_s03_c010-full_annotation.xml', True, True)
+
+    init_percent = .25
+    init_images = int(len(ims) * init_percent)
+    first_eval_image = init_images + 1
+
+    modes = ['mog', 'knn', 'gmg']
+    model_params = {
+        'mog': {
+            'varThreshold': 32
+        },
+        'knn': {
+            'dist2Threshold': 350
+        },
+        'gmg': {
+            'initializationFrames': init_images
+        }
+
+    }
+
+    for mode in modes:
+        bg_sub = bg_estimation(mode, **model_params[mode])
+        det_bb = []
+        for i, im in enumerate(ims):
+            im = cv2.imread(im)
+            fg = bg_sub.apply(im)
+            # cv2.imshow("a", fg)
+            # cv2.waitKey(0)
+
+            if i >= first_eval_image:
+
+                contours, _ = cv2.findContours(fg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+                for contour in contours:
+                    # Open cv documentation:  (x,y) be the top-left coordinate
+                    # of the rectangle and (w,h) be its width and height.
+                    (x, y, w, h) = cv2.boundingRect(contour)
+                    if w > 10 and h > 10:
+                        det_bb.append([i, 'car', 0, x, y, x + w, y + h])
+
+        ap = calculate_ap(det_bb, gt_bb, init_images + 1, len(ims), mode='area')
+        print("{} - {}".format(mode, ap))
 
 
 if __name__ == '__main__':
